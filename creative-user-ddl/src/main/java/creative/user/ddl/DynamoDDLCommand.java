a675240da4b0886c6dfb85f7838dc04708e05ad9
Vihung Marathe <vmarathe@deliverycube.com>
2018-06-17 23:52:33 +0100

    Remove @Override annotation for interface methods

--- creative-user-ddl/src/main/java/creative/user/ddl/DynamoDDLCommand.java
+++ creative-user-ddl/src/main/java/creative/user/ddl/DynamoDDLCommand.java
@@ -23,116 +23,118 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
  *
  */
 public abstract class DynamoDDLCommand {
-    // Logger for this class
-    private static final Log log = LogFactory.getLog(DynamoDDLCommand.class);
+  // Logger for this class
+  private static final Log log = LogFactory.getLog(DynamoDDLCommand.class);
 
-    protected AmazonDynamoDB client;
+  protected AmazonDynamoDB client;
 
-    private final String CONFIG_FILE = "/DynamoDBConfig.properties";
-    protected boolean initialised = false;
+  private final String CONFIG_FILE = "/DynamoDBConfig.properties";
+  protected boolean initialised = false;
 
-    /**  */
-    public DynamoDDLCommand() {
-        super();
+  /**  */
+  public DynamoDDLCommand() {
+    super();
+  }
+
+  public final boolean clean() {
+    boolean success;
+    try {
+      preClean();
+      doClean();
+      postClean();
+
+      success = true;
+    } catch (final Exception e) {
+      log.fatal(e);
+      success = false;
+    }
+    return success;
+  }
+
+  private AWSCredentialsProvider createAmazonAWSCredentialsProvider(final String amazonAWSAccessKey,
+      final String amazonAWSSecretKey) {
+    final AWSCredentialsProvider provider = new AWSCredentialsProvider() {
+      private AWSCredentials mCredentials = new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+
+      public AWSCredentials getCredentials() {
+        return mCredentials;
+
+      }
+
+      public void refresh() {
+        mCredentials = new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+      }
+    };
+    return provider;
+  }
+
+  private AmazonDynamoDB createAmazonDynamoDB(final String amazonDynamoDBEndpoint, final String amazonAWSAccessKey,
+      final String amazonAWSSecretKey) {
+    final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+        .withCredentials(createAmazonAWSCredentialsProvider(amazonAWSAccessKey, amazonAWSSecretKey))
+        .withEndpointConfiguration(
+            new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, Regions.DEFAULT_REGION.getName()))
+        .build();
+
+    return client;
+  }
+
+  protected abstract void doClean();
+
+  protected abstract void doRun();
+
+  public abstract String getName();
+
+  public void init() throws IOException {
+    final Properties dynamoDBConfig = loadDynamoDBConfig();
+
+    final String amazonAWSAccessKey = dynamoDBConfig.getProperty("amazonAWSAccessKey");
+    final String amazonAWSSecretKey = dynamoDBConfig.getProperty("amazonAWSSecretKey");
+    final String amazonDynamoDBEndpoint = dynamoDBConfig.getProperty("amazonDynamoDBEndpoint");
+
+    client = createAmazonDynamoDB(amazonDynamoDBEndpoint, amazonAWSAccessKey, amazonAWSSecretKey);
+    initialised = true;
+  }
+
+  private Properties loadDynamoDBConfig() throws IOException {
+    final Properties dynamoDBConfig = new Properties();
+    final InputStream stream = getClass().getResourceAsStream(CONFIG_FILE);
+    dynamoDBConfig.load(stream);
+    return dynamoDBConfig;
+  }
+
+  protected void postClean() {
+  }
+
+  private final void postRun() {
+  }
+
+  private final void preClean() throws IOException {
+    if (!initialised) {
+      init();
+    }
+  }
+
+  protected void preRun() throws IOException {
+    if (!initialised) {
+      init();
     }
 
-    public final boolean clean() {
-        boolean success;
-        try {
-            preClean();
-            doClean();
-            postClean();
+  }
 
-            success = true;
-        } catch (final Exception e) {
-            log.fatal(e);
-            success = false;
-        }
-        return success;
+  public final boolean run() {
+    boolean success;
+    try {
+      preRun();
+      doRun();
+      postRun();
+
+      success = true;
+    } catch (final IOException e) {
+      log.fatal(e);
+      success = false;
     }
-
-    private AWSCredentialsProvider createAmazonAWSCredentialsProvider(final String amazonAWSAccessKey, final String amazonAWSSecretKey) {
-        final AWSCredentialsProvider provider = new AWSCredentialsProvider() {
-            private AWSCredentials mCredentials = new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
-
-            @Override
-            public AWSCredentials getCredentials() {
-                return mCredentials;
-
-            }
-
-            @Override
-            public void refresh() {
-                mCredentials = new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
-            }
-        };
-        return provider;
-    }
-
-    private AmazonDynamoDB createAmazonDynamoDB(final String amazonDynamoDBEndpoint, final String amazonAWSAccessKey, final String amazonAWSSecretKey) {
-        final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withCredentials(createAmazonAWSCredentialsProvider(amazonAWSAccessKey, amazonAWSSecretKey))
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, Regions.DEFAULT_REGION.getName())).build();
-
-        return client;
-    }
-
-    protected abstract void doClean();
-
-    protected abstract void doRun();
-
-    public abstract String getName();
-
-    public void init() throws IOException {
-        final Properties dynamoDBConfig = loadDynamoDBConfig();
-
-        final String amazonAWSAccessKey = dynamoDBConfig.getProperty("amazonAWSAccessKey");
-        final String amazonAWSSecretKey = dynamoDBConfig.getProperty("amazonAWSSecretKey");
-        final String amazonDynamoDBEndpoint = dynamoDBConfig.getProperty("amazonDynamoDBEndpoint");
-
-        client = createAmazonDynamoDB(amazonDynamoDBEndpoint, amazonAWSAccessKey, amazonAWSSecretKey);
-        initialised = true;
-    }
-
-    private Properties loadDynamoDBConfig() throws IOException {
-        final Properties dynamoDBConfig = new Properties();
-        final InputStream stream = getClass().getResourceAsStream(CONFIG_FILE);
-        dynamoDBConfig.load(stream);
-        return dynamoDBConfig;
-    }
-
-    protected void postClean() {
-    }
-
-    private final void postRun() {
-    }
-
-    private final void preClean() throws IOException {
-        if (!initialised) {
-            init();
-        }
-    }
-
-    protected void preRun() throws IOException {
-        if (!initialised) {
-            init();
-        }
-
-    }
-
-    public final boolean run() {
-        boolean success;
-        try {
-            preRun();
-            doRun();
-            postRun();
-
-            success = true;
-        } catch (final IOException e) {
-            log.fatal(e);
-            success = false;
-        }
-        return success;
-    }
+    return success;
+  }
 
 }
--- creative-user-ddl/src/main/java/creative/user/ddl/DynamoDDLCommandRunner.java
+++ creative-user-ddl/src/main/java/creative/user/ddl/DynamoDDLCommandRunner.java
@@ -16,36 +16,36 @@ import creative.user.ddl.table.UserDDL;
  */
 public class DynamoDDLCommandRunner {
 
-    public static void cleanAll() {
-        for (final DynamoDDLCommand command : getCommands()) {
-            boolean success;
-            success = command.clean();
-            System.out.println(command.getName() + " Cleaned " + (success ? "successfully" : "unsuccessfully"));
-        }
+  public static void cleanAll() {
+    for (final DynamoDDLCommand command : getCommands()) {
+      boolean success;
+      success = command.clean();
+      System.out.println(command.getName() + " Cleaned " + (success ? "successfully" : "unsuccessfully"));
     }
+  }
 
-    public static void createAll() {
-        for (final DynamoDDLCommand command : getCommands()) {
-            boolean success;
-            success = command.run();
-            System.out.println(command.getName() + " Cleaned " + (success ? "successfully" : "unsuccessfully"));
-        }
+  public static void createAll() {
+    for (final DynamoDDLCommand command : getCommands()) {
+      boolean success;
+      success = command.run();
+      System.out.println(command.getName() + " Created " + (success ? "successfully" : "unsuccessfully"));
     }
+  }
 
-    private final static List<DynamoDDLCommand> getCommands() {
-        final List<DynamoDDLCommand> commands = new ArrayList<DynamoDDLCommand>();
-        commands.add(new UserDDL());
-        commands.add(new UserCredentialsDDL());
-        commands.add(new AccessTokenDDL());
-        return commands;
-    }
+  private final static List<DynamoDDLCommand> getCommands() {
+    final List<DynamoDDLCommand> commands = new ArrayList<DynamoDDLCommand>();
+    commands.add(new UserDDL());
+    commands.add(new UserCredentialsDDL());
+    commands.add(new AccessTokenDDL());
+    return commands;
+  }
 
-    /**
-     * @param args
-     */
-    public static void main(final String[] args) {
-        cleanAll();
-        createAll();
-    }
+  /**
+   * @param args
+   */
+  public static void main(final String[] args) {
+    cleanAll();
+    createAll();
+  }
 
 }
